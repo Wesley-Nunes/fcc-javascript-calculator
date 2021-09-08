3f71e2c5336489fa5a5358d64cef3c3220662482
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-09-08 16:48:34 -0300

    style: Add style to Button component
    Styled Components create the style of the Button component.
    StyledButton receives props to change the style.
    Prop Types and default values document the props in the index.jsx file
    inside of Button folder.
    The Button component receives CSS props.

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Style from './style';
 
-function Button({ children }) {
-  return <button type="button">{children}</button>;
+function Button({
+  fontColor, buttonColor, width, children,
+}) {
+  return (
+    <Style
+      type="button"
+      fontColor={fontColor}
+      buttonColor={buttonColor}
+      width={width}
+    >
+      {children}
+    </Style>
+  );
 }
 
 Button.propTypes = {
+  fontColor: PropTypes.string,
+  buttonColor: PropTypes.string,
+  width: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 Button.defaultProps = {
+  fontColor: 'hsl(0, 0%, 100%)',
+  buttonColor: 'hsl(196, 49%, 8%)',
+  width: '6.4rem',
   children: 'error',
 };
 
