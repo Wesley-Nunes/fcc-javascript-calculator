f2cd15de69bbbacaa48ff417b55f3958ee624b61
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-09-08 20:02:01 -0300

    feat: Create the MidContainer component
    Make one container with two others containers,
    (NumbersContainer and ActionsContainerTwo).

---
+++ src/components/MidContainer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NumbersContainer from '../NumbersContainer';
+import ActionsContainerTwo from '../ActionsContainerTwo';
+
+function MidContainer() {
+  return (
+    <div>
+      <div>
+        <NumbersContainer />
+      </div>
+      <div>
+        <ActionsContainerTwo />
+      </div>
+    </div>
+  );
+}
+
+export default MidContainer;
