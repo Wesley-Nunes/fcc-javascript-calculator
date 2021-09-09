f53ebf941e864c60bf43d7cd16a61594423b4a9a
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-09-09 15:56:35 -0300

    test: Output component render '0'(defaultValue)

---
+++ src/components/Output/index.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Output from './index';
+
+describe('Output', () => {
+  const defaultValue = '0';
+  render(<Output expression={defaultValue} />);
+
+  test("Output component render '0'(defaultValue)", () => {
+    expect(screen.getByText(defaultValue)).toBeInTheDocument();
+  });
+});
