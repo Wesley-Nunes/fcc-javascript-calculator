876088429ce78f8288d94bec250ef147c3eb0355
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-09-10 10:51:18 -0300

    feat: Create function handleMath
    handleMath receives the value of the clicked button and
    insert in the Current component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GlobalStyles from './GlobalStyles';
 import StyledMain from './App.style';
 import {
   BotContainer, MidContainer, TopContainer, History, Current,
 } from './components';
+import { ButtonValueProvider, useButtonValue } from './context/ButtonValue';
 
 function Display() {
+  const { getButtonValue } = useButtonValue();
+  const currentValue = getButtonValue();
+
+  function handleMath(value) {
+    const [memoValue, setMemoValue] = useState({
+      current: '',
+      history: '',
+    });
+
+    useEffect(() => {
+      const values = `${memoValue.current}${value.toString()}`;
+      setMemoValue({
+        current: values,
+        history: '',
+      });
+    }, [value]);
+
+    return memoValue.current;
+  }
+
+  const current = handleMath(currentValue);
+
   return (
     <>
       <History />
-      <Current />
+      <Current value={current} />
     </>
   );
 }
@@ -29,8 +52,10 @@ function App() {
     <>
       <GlobalStyles />
       <StyledMain>
-        <Display />
-        <ButtonsContainer />
+        <ButtonValueProvider>
+          <Display />
+          <ButtonsContainer />
+        </ButtonValueProvider>
       </StyledMain>
     </>
   );
