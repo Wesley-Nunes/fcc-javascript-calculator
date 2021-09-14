cc3cf021f6ddd250347c41d1bcfdf320e12755ff
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-09-14 10:38:40 -0300

    feat: Display component handle the side effect of value

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GlobalStyles from './GlobalStyles';
 import StyledMain from './App.style';
 import {
@@ -8,19 +8,23 @@ import { ButtonValueProvider, useButtonValue } from './context/ButtonValue';
 import handleMath from './utils/handleMath';
 
 function Display() {
-  const { getButtonValue } = useButtonValue();
+  const { getButtonValue, resetValues } = useButtonValue();
   const currentValue = getButtonValue();
-  const oldValue = {
-    current: '10',
+  const [value, setValue] = useState({
+    current: '0',
     history: '',
-  };
+  });
+  const newValue = handleMath(value, currentValue);
 
-  const newValue = handleMath(oldValue, currentValue);
+  useEffect(() => {
+    setValue(newValue);
+    resetValues();
+  }, [currentValue]);
 
   return (
     <>
       <History />
-      <Current value={newValue.current} />
+      <Current value={value.current} />
     </>
   );
 }
