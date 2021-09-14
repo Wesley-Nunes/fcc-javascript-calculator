bf96e9054448898e38c57f93b201eed1b2d2e28e
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-09-14 13:56:48 -0300

    refactor: Values are global to avoid duplicated code

--- src/utils/handleMath.test.js
+++ src/utils/handleMath.test.js
@@ -1,54 +1,47 @@
 import handleMath from './handleMath';
 
 describe('handleMath', () => {
-  test('If zero is the only number in value, return the input in string type', () => {
-    const value = {
+  const values = {
+    current: '42',
+    history: '',
+  };
+
+  test('If zero is the only number in localValue.current, return the input in string type', () => {
+    const localValue = {
       current: '0',
       history: '',
     };
     const inputs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     inputs.map((input) => {
-      const result = handleMath(value, input);
+      const result = handleMath(localValue, input);
       return expect(result.current).toBe(`${input}`);
     });
   });
-  test('If value.current is different from zero, return the value.current with input in one string', () => {
-    const value = {
-      current: '20',
-      history: '',
-    };
+  test('If values.current is different from zero, return the values.current with input in one string', () => {
     const inputs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     inputs.map((input) => {
-      const result = handleMath(value, input);
-      return expect(result.current).toBe(`20${input}`);
+      const result = handleMath(values, input);
+      return expect(result.current).toBe(`42${input}`);
     });
   });
-  test('If input is "C", return result.current equals "0", and result.history as empty string', () => {
-    const value = {
-      current: '55-14',
-      history: '',
-    };
+  test('If input is "C", return values.current equals "0", and values.history as empty string', () => {
     const input = 'C';
-    const result = handleMath(value, input);
+    const result = handleMath(values, input);
     expect(result.current).toBe('0');
     expect(result.history).toBe('');
   });
-  test('If the input is "+/-" and if the value.current is a positive number, return the value.current in negative form', () => {
-    const value = {
-      current: '20',
-      history: '',
-    };
+  test('If the input is "+/-" and if the values.current is a positive number, return the values.current in negative form', () => {
     const input = '+/-';
-    const result = handleMath(value, input);
-    expect(result.current).toBe('(-20');
+    const result = handleMath(values, input);
+    expect(result.current).toBe('(-42');
   });
-  test('If the input is "+/-" and if the value.current is a negative number, return the value.current in positive form', () => {
-    const value = {
+  test('If the input is "+/-" and if the values.current is a negative number, return the values.current in positive form', () => {
+    const localValue = {
       current: '(-42',
       history: '',
     };
     const input = '+/-';
-    const result = handleMath(value, input);
+    const result = handleMath(localValue, input);
     expect(result.current).toBe('42');
   });
 });
